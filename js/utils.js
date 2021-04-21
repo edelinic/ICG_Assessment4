d64ee95af41e5fd94cf6da898ddb9db713c07c6b
Lachie Kimber <lachiekimber@icloud.com>
2021-04-21 13:43:24 +1000

    Function to get random weighted values
    This function can handle weighted random numbers.

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,18 @@
 export function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export function getRandomWeighted(results, weights) {
+    var num = Math.random(),
+        s = 0,
+        lastIndex = weights.length - 1;
+
+    for (var i = 0; i < lastIndex; ++i) {
+        s += weights[i];
+        if (num < s) {
+            return results[i];
+        }
+    }
+
+    return results[lastIndex];
+};
